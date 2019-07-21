5de744d152da47e30026347e17a5e9d8a6541311
Kyou13 <kyou1013@gmail.com>
2019-07-21 11:40:12 +0900

    [fix] models imagesize

--- src/models.py
+++ src/models.py
@@ -5,7 +5,7 @@ from torch import nn
 class Discriminator(nn.Module):
   def __init__(self, imageSize):
     super(Discriminator, self).__init__()
-    self.linear1 = nn.Linear(pow(imageSize, 2), 512)
+    self.linear1 = nn.Linear(imageSize, 512)
     self.linear2 = nn.Linear(512, 256)
     self.linear3 = nn.Linear(256, 1)
     self.LeakyReLU = nn.LeakyReLU(0.2, inplace=True)
@@ -28,7 +28,7 @@ class Generator(nn.Module):
     self.bn2 = nn.BatchNorm1d(512, 0.8)
     self.linear4 = nn.Linear(512, 1024)
     self.bn3 = nn.BatchNorm1d(1024, 0.8)
-    self.linear5 = nn.Linear(1024, pow(imageSize, 2))
+    self.linear5 = nn.Linear(1024, imageSize)
     self.LeakyReLU = nn.LeakyReLU(0.2, inplace=True)
 
   def forward(self, x):
