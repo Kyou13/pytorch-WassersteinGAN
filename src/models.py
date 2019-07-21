e1bf29b2673d21647f6bccc0a56b05f96589c85b
Kyou13 <kyou1013@gmail.com>
2019-07-21 15:15:49 +0900

    [fix] typo

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ class Discriminator(nn.Module):
     self.linear1 = nn.Linear(imageSize, 512)
     self.linear2 = nn.Linear(512, 256)
     self.linear3 = nn.Linear(256, 1)
-    self.LeakyReLU = nn.LeakyReLU(0.2, inplace=True)
+    self.leakyReLU = nn.LeakyReLU(0.2, inplace=True)
 
   def forward(self, x):
     x = self.leakyReLU(self.linear1(x))
@@ -20,7 +20,6 @@ class Discriminator(nn.Module):
 class Generator(nn.Module):
   def __init__(self, nz, imageSize):
     super(Generator, self).__init__()
-    self.imageSize = imageSize
     self.linear1 = nn.Linear(nz, 128)
     self.linear2 = nn.Linear(128, 256)
     self.bn1 = nn.BatchNorm1d(256, 0.8)
@@ -29,12 +28,12 @@ class Generator(nn.Module):
     self.linear4 = nn.Linear(512, 1024)
     self.bn3 = nn.BatchNorm1d(1024, 0.8)
     self.linear5 = nn.Linear(1024, imageSize)
-    self.LeakyReLU = nn.LeakyReLU(0.2, inplace=True)
+    self.leakyReLU = nn.LeakyReLU(0.2, inplace=True)
 
   def forward(self, x):
-    x = self.LeakyReLU(self.linear1(x))
-    x = self.LeakyReLU(self.bn1(self.linear2(x)))
-    x = self.LeakyReLU(self.bn2(self.linear3(x)))
-    x = self.LeakyReLU(self.bn3(self.linear4(x)))
+    x = self.leakyReLU(self.linear1(x))
+    x = self.leakyReLU(self.bn1(self.linear2(x)))
+    x = self.leakyReLU(self.bn2(self.linear3(x)))
+    x = self.leakyReLU(self.bn3(self.linear4(x)))
     x = torch.tanh(self.linear5(x))
     return x
